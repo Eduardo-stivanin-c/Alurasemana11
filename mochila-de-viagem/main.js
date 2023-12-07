fcda83d56c88cbbf685fa23a0ca50e79461e2179
Eduardo-stivanin-c <1sfr2d@gmail.com>
2023-12-07 13:25:56 -0300

    terminada semna 11

--- mochila-de-viagem/main.js
+++ mochila-de-viagem/main.js
@@ -1,46 +1,39 @@
-// Operador lógico que retorna com dados salvos, ou string vazia, utilizando localStorage.getItem, modificando o valor de `string` com JSON.parse()
-"use strick"
-const form = document.getElementById("novoItem") 
+const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
-const itens = JSON.parse(localStorage.getItem("itens")) || []   
+const itens = JSON.parse(localStorage.getItem("itens")) || []
 
-//console.log(existe)
-// Uso do forEach para que todos os itens já escritos na lista sejam mantidos ao atualizar a página 
-itens.forEach( (elemento) => {    
+itens.forEach( (elemento) => {
     criaElemento(elemento)
-} )     
+} )
 
-// Refatoração do addEventListener para receber as funções extras da função criaElemento
-form.addEventListener("submit", (evento) => {   
-    evento.preventDefault()            
-    const existe=itens.find(elemento =>elemento.nome===nome.value)
-    
-    console.log(existe)
-//se id existe reotna o mesmo id senão reotna o  posiçõa do elentoe
-const itemAtual = {
-    "nome": nome.value,
-    "quantidade": quantidade.value,
-    }
-//se existe
-    if (existe) {
-      itemAtual.id=item.id
-      console.log(existe.id)
-    }
-    else{
-      itemAtual.id=item.length
-      criaElemento(itemAtual)
-      itens.push(itemAtual)
-    }
-
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
 
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
-    
+    const existe = itens.find( elemento => elemento.nome === nome.value )
 
-    criaElemento(itemAtual)
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
 
-    itens.push(itemAtual)
+    if (existe) {
+        itemAtual.id = existe.id
+        
+        atualizaElemento(itemAtual)
+
+        itens(itens.findIndex(elemento => elemento.id===existe.id)) = itemAtual
+    } else {
+                        //se id atual < -1  
+
+        itemAtual.id =itens[itens.length-1]?(itens[itens.length-1]).id +1:0
+
+        criaElemento(itemAtual)
+
+        itens.push(itemAtual)
+    }
 
     localStorage.setItem("itens", JSON.stringify(itens))
 
@@ -48,23 +41,45 @@ const itemAtual = {
     quantidade.value = ""
 })
 
-// Refatoração da função `criaElemento` para que possua apenas a função que faça sentido ao nome. 
-
-function criaElemento(item) {  
-    const novoItem = document.createElement('li')
+function criaElemento(item) {
+    const novoItem = document.createElement("li")
     novoItem.classList.add("item")
 
-    const numeroItem = document.createElement('strong')
+    const numeroItem = document.createElement("strong")
     numeroItem.innerHTML = item.quantidade
-    //pode inventar quanarquer atributo até farofa
-    numeroItem.dataset.id=item.id
-
+    numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem)
-
-
-
-
-novoItem.innerHTML += item.nome
-
+    
+    novoItem.innerHTML += item.nome
+novoItem.appendChild(botaoDeleta())
     lista.appendChild(novoItem)
 }
+
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
+
+//criar função botão 
+
+function botaoDeleta(id){
+    const elementoBotao=document.createElement("button")
+    elementoBotao.innerText="X"
+    
+    elementoBotao.addEventListener("click",function(){
+        deleteElemento(this.parentNode,id)
+    })
+    return elementoBotao
+    }
+
+    function deleteElemento(tag,id) {
+        tag.remove()   
+        //remover um item do array(matriz)
+                    //"O q\ queremos remover                   "     
+        itens.splice(itens.findIndex(elemento=>elemento.id===id),1)
+         
+         console.log(itens)
+        //escrever no LocalStorage
+        localStorage.setItem("itens", JSON.stringify(itens))
+   
+    }
+       
---
+++ mochila-de-viagem/se_existe/se_existe.js
@@ -0,0 +1,15 @@
+/*const existe = itens.find( elemento => elemento.nome === nome.value )
+/*
+ if (existe) {
+        ...código omitido
+    } else {
+        ...código omitido
+    }
+
+
+    console.log(existe)
+    if (existe) {
+        ...código omitido
+    } else {
+        ...código omitido
+    }*/
